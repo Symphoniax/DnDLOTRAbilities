6f10a3c58902252c1a5ed7d7583a222f9473bf4d
Symphoniax <gmatty95@gmail.com>
2019-03-19 11:58:22 +0100

    Added class list (not implemented)

--- app/src/main/java/com/symphoniax/dndlotrabilities/activities/MainActivity.java
+++ app/src/main/java/com/symphoniax/dndlotrabilities/activities/MainActivity.java
@@ -8,18 +8,46 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 import com.symphoniax.dndlotrabilities.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
 
+    private ArrayList<String> classi = new ArrayList<>(
+            Arrays.asList("Classe 1", "Classe 2", "Classe 3", "Classe 4", "Classe 5", "Classe 6", "Classe 7"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.class_list);
+
+        CustomAdapter adapter = new CustomAdapter(
+                this,
+                R.layout.item,
+                classi);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent,
+                                    View view,
+                                    int position,
+                                    long id) {
+                Toast.makeText(MainActivity.this,
+                        classi.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
